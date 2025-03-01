4a3e1f28f3c92144cae8bf5cda44ffebe4b63807
yingqi0607 <66065012+yingqi0607@users.noreply.github.com>
2025-03-01 16:28:04 +0800

    payments init

--- Online Banking App Spring Boot/src/main/java/com/beko/DemoBank_v1/DemoBankV1Application.java
+++ Online Banking App Spring Boot/src/main/java/com/beko/DemoBank_v1/DemoBankV1Application.java
@@ -3,7 +3,7 @@ package com.beko.DemoBank_v1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.beko.DemoBank_v1")
 public class DemoBankV1Application {
 
 	public static void main(String[] args) {
